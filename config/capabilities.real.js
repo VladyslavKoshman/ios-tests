7f4812b807033b70f797205a377e40404fbf2091
VladyslavKoshman <vlad@masterpilot.aero>
2025-11-13 13:05:20 +0200

    config chng

--- config/capabilities.real.js
+++ config/capabilities.real.js
@@ -2,11 +2,12 @@ export const iosRealCaps = {
   platformName: 'iOS',
   deviceName: 'accountant', // имя устройства из Xcode (или `idevice_id -l`)
   udid: '00008110-000E28522206401E', // UDID твоего телефона (узнать командой выше)
-  platformVersion: '26.0.1', // версия iOS на телефоне
+  platformVersion: '17.0.1', // версия iOS на телефоне
   automationName: 'XCUITest',
   bundleId: 'aero.sofloworks.masterpilot', // твой app bundle id
-  xcodeOrgId: 'SOFLOWORKS INC', // Apple Team ID
+  xcodeOrgId: '6CGNHJD5VX', // Apple Team ID
   xcodeSigningId: 'Apple Development',
-  updatedWDABundleId: 'com.facebookvlad.WebDriverAgentRunner.xctrunner', // уникальный bundle для WDA
+  //updatedWDABundleId: 'com.facebookvlad.WebDriverAgentRunner.xctrunner', // уникальный bundle для WDA
+  updatedWDABundleId: 'com.vlad.WebDriverAgentRunner', // уникальный bundle для WDA
   noReset: true,
 };
