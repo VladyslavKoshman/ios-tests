c1f5ff4fa3946c842d172377e3e618859e07b80c
VladyslavKoshman <vlad@masterpilot.aero>
2025-11-21 16:57:34 +0200

    all works

--- config/capabilities.sim.js
+++ config/capabilities.sim.js
@@ -1,9 +1,8 @@
 export const iosSimCaps = {
-  platformName: 'iOS',
-  deviceName: 'iPhone 16e',
-  platformVersion: '26.1',
-  automationName: 'XCUITest',
-  //app: '/Users/accountant/Downloads/MasterPilot 2.app',
-  app: '/Users/accountant/Downloads/Telegram Desktop/MasterPilot.app',
-  noReset: true,
+  platformName: 'iOS', // стандартный W3C capability, без префикса
+  //'appium:deviceName': 'iPhone 16e',
+  'appium:platformVersion': '26.1',
+  'appium:automationName': 'XCUITest',
+  'appium:app': '/Users/accountant/Downloads/Telegram Desktop/MasterPilot.app',
+  'appium:noReset': true,
 };
